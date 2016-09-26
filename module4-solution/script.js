82a884e8bf0a96486befcd002f44c3e3c260b197
Will Sterling <will.sterling@wowza.com>
2016-09-25 23:31:26 -0600

    Fixed error.

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -51,7 +51,7 @@ for (var name in names) {
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
-  var firstLetter = name.charAt(0).toLowerCase()
+  var firstLetter = names[name].charAt(0).toLowerCase()
 
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
